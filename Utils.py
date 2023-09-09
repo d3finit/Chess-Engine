a2ee054ab66a76f9d9093ad30037784a74c6dfc8
def__init__ <liam.outlaw@gmail.com>
2023-09-08 20:18:14 -0700

    some fixes and elo improvements
    - better material checks (alpha / beta pruning [its bad rn tho])
    - moved some code
    - removed minimax bc it was hanging pieces (throwing by leaving pieces unprotected)
    - less memory usage by making some functions 1 liners (no vars used)
    - BUG: computer move breaks at some point, no traceback error

---
+++ Utils.py
@@ -0,0 +1,49 @@
+# tuple sorter
+def sort_tuple(tup):
+    tup.sort(key=lambda x: x[1])
+    return tup
+
+# bops the board to be intagers of peive values or something I forgot lol
+def convert_to_int(board_to_convert):
+    return [
+        ['♚♛♜♝♞♟⭘♙♘♗♖♕♔'.index(c) - 6 for c in row.split()]
+        for row in board_to_convert.unicode().split('\n')
+    ]
+
+# converts the board to ANASI characters
+def convert_to_anasi(board_to_convert):
+    mapped = {
+        1: "♟",  # White Pawn
+        -1: "♙",  # Black Pawn
+        2: "♞",  # White Knight
+        -2: "♘",  # Black Knight
+        3: "♝",  # White Bishop
+        -3: "♗",  # Black Bishop
+        4: "♜",  # White Rook
+        -4: "♖",  # Black Rook
+        5: "♛",  # White Queen
+        -5: "♕",  # Black Queen
+        6: "♚",  # White King
+        -6: "♔"  # Black King
+    }
+    for j in range(len(board_to_convert)):
+        for h in range(len(board_to_convert[j])):
+            if board_to_convert[j][h] != 0:
+                board_to_convert[j][h] = mapped[board_to_convert[j][h]]
+            else:
+                board_to_convert[j][h] = "⭘"
+
+    return board_to_convert
+
+# prints a formatted board
+def print_board(board_to_convert):
+    board_to_convert = convert_to_anasi(convert_to_int(board_to_convert))
+
+    print("  |A|B|C|D|E|F|G|H|")
+
+    for j in range(len(board_to_convert)):
+        line = f"|{str(8 - j)}|"
+        for h in range(len(board_to_convert[j])):
+            line = line + board_to_convert[j][h] + " "
+
+        print(line)
--- main.py
+++ main.py
@@ -5,118 +5,22 @@
 # ------------------------------------
 
 # import required libs
-import chess
-import chess.svg
-import chess.pgn
-import chess.polyglot
-import chess.engine
+import chess, chess.svg, chess.pgn, chess.polyglot, chess.engine
 from Console import *
 import os
 from rich.progress import track, Progress
-import random
 from rich.prompt import Prompt
 import time
-
-
-# tuple sorter
-def sort_tuple(tup):
-    tup.sort(key=lambda x: x[1])
-    return tup
-
-
-def convert_to_int(board_to_convert):
-    return [
-        ['♚♛♜♝♞♟⭘♙♘♗♖♕♔'.index(c) - 6 for c in row.split()]
-        for row in board_to_convert.unicode().split('\n')
-    ]
-
-
-def convert_to_anasi(board_to_convert):
-    mapped = {
-        1: "♟",  # White Pawn
-        -1: "♙",  # Black Pawn
-        2: "♞",  # White Knight
-        -2: "♘",  # Black Knight
-        3: "♝",  # White Bishop
-        -3: "♗",  # Black Bishop
-        4: "♜",  # White Rook
-        -4: "♖",  # Black Rook
-        5: "♛",  # White Queen
-        -5: "♕",  # Black Queen
-        6: "♚",  # White King
-        -6: "♔"  # Black King
-    }
-    for j in range(len(board_to_convert)):
-        for h in range(len(board_to_convert[j])):
-            if board_to_convert[j][h] != 0:
-                board_to_convert[j][h] = mapped[board_to_convert[j][h]]
-            else:
-                board_to_convert[j][h] = "⭘"
-
-    return board_to_convert
-
-
-def print_good(board_to_convert):
-    board_to_convert = convert_to_anasi(convert_to_int(board_to_convert))
-
-    print("  |A|B|C|D|E|F|G|H|")
-
-    for j in range(len(board_to_convert)):
-        line = f"|{str(8 - j)}|"
-        for h in range(len(board_to_convert[j])):
-            line = line + board_to_convert[j][h] + " "
-
-        print(line)
+from Utils import sort_tuple, convert_to_int, print_board  # local lib for stuff
 
 
 def get_material(board_to_get):
     board_to_get = convert_to_int(board_to_get)
-
     num = 0
-
     for j in range(len(board_to_get)):
         for h in range(len(board_to_get[j])):
             num += int(board_to_get[j][h])
-
     return num
-
-
-# Move Generation
-def generate_moves():
-    moves = []
-    for move_list in board.legal_moves:
-        moves.append(move_list)
-    return moves
-
-
-# Move Evaluation
-def evaluate_move():
-    # Simple evaluation function: Return a random score between -1 and 1
-    eval_score = random.uniform(-1, 1)
-    return eval_score
-
-
-# Search Algorithm
-def minimax(depth, maximizing_player):
-    if depth == 0 or board.is_game_over():
-        return evaluate_move()
-
-    if maximizing_player:
-        max_eval = float("-inf")
-        for move in generate_moves():
-            board.push(move)
-            eval = minimax(depth - 1, False)
-            board.pop()
-            max_eval = max(max_eval, eval)
-        return max_eval
-    else:
-        min_eval = float("inf")
-        for move in generate_moves():
-            board.push(move)
-            eval = minimax(depth - 1, True)
-            board.pop()
-            min_eval = min(min_eval, eval)
-        return min_eval
 
 
 def recurse_checkmate(board, movestack, limit):
@@ -167,65 +71,27 @@ def recurse_checkmate_2(board, movestack, limit, move, progress):
     return movestack
 
 
-def recurse_material(board, movestack, limit, full_board, blocked_moves):
-    with Progress() as progress:
-        d1 = progress.add_task(f"Depth {limit} checking...", total=board.legal_moves.count())
-        for temp_move in board.legal_moves:
-            if len(movestack) >= 1:
-                pass
+def new_recurse_material(board, movestack, depth):
+    for temp_move in board.legal_moves:
+        get_material(board)
 
-            else:
-                temp_board = chess.Board(fen=board.fen())
+        temp_board = chess.Board()
+        temp_board.set_fen(board.fen())
 
-                temp_board.push(temp_move)
+        temp_board.push(temp_move)
 
-                if get_material(temp_board) <= get_material(board):
-                    blocked_moves.append(move)
+        if get_material(temp_board) > get_material(board):
+            movestack.push([temp_move, 1000])
+            movestack = movestack + new_recurse_material(board, movestack, depth - 1)
+            print(movestack, depth)
 
-                progress.update(d1, advance=1)
-                temp = recurse_material_2(temp_board, movestack, limit - 1, temp_move, progress, full_board,
-                                          blocked_moves)
-                movestack, blocked_moves = movestack + temp[0], blocked_moves + temp[1]
-
-    return movestack, blocked_moves
-
-
-def recurse_material_2(board, movestack, limit, move, progress, full_board, blocked_moves):
-    if limit == 0:
-        if get_material(full_board) <= get_material(board):
-            allmoves.append([move, 1000])
-    else:
-        if limit == 1 or limit == 2:
-            pass
-        else:
-            d1 = progress.add_task(f"Depth {limit} checking...", total=board.legal_moves.count())
-
-        if len(movestack) >= 1:
-            pass
-
-        else:
-            for temp_move in board.legal_moves:
-                temp_board = chess.Board()
-                temp_board.set_fen(board.fen())
-
-                temp_board.push(temp_move)
-
-                if get_material(temp_board) <= get_material(board):
-                    blocked_moves.append(move)
-
-                if limit == 1 or limit == 2:
-                    pass
-                else:
-                    progress.update(d1, advance=1)
-
-                    recurse_material_2(temp_board, movestack, limit - 1, move, progress, full_board, blocked_moves)
-    return movestack, blocked_moves
+    return movestack
 
 
 if __name__ == "__main__":
     # PERMVARS
     MATE_SCAN_DEPTH = 4
-    MATERIAL_SCAN_DEPTH = 5
+    MATERIAL_SCAN_DEPTH = 15
     MINIMAX_DEPTH = 3
 
     # load stonksfish
@@ -245,7 +111,7 @@ if __name__ == "__main__":
         print(f"White is up {get_material(board)}")
     else:
         print("Tied in material")
-    print_good(board)
+    print_board(board)
 
     while True:
         # get user move
@@ -267,7 +133,7 @@ if __name__ == "__main__":
             print(f"White is up {get_material(board)}")
         else:
             print("Tied in material")
-        print_good(board)
+        print_board(board)
 
         start = time.time()
 
@@ -275,30 +141,14 @@ if __name__ == "__main__":
         allmoves = []
         blocked_moves = []
 
-        best_score = float("-inf")
-        best_move = None
-        for move in track(generate_moves(), description="Minimax scanning..."):
-            board.push(move)
-            score = minimax(MINIMAX_DEPTH, False)  # Depth 3 search
-            board.pop()
-            if score > best_score:
-                best_score = score
-                best_move = move
-
-        allmoves.append([best_move, 100000])
-
         for i in range(1, MATE_SCAN_DEPTH + 1):
             if len(allmoves) >= 1:
                 pass
             else:
                 allmoves = allmoves + recurse_checkmate(board, allmoves, i)
 
-        for i in range(1, MATERIAL_SCAN_DEPTH + 1):
-            if len(allmoves) >= 1:
-                pass
-            else:
-                temp = recurse_material(board, allmoves, i, board, blocked_moves)
-                allmoves, blocked_moves = allmoves + temp[0], blocked_moves + temp[1]
+        # better material recurse with ALPHA BETA PRUNING (its trash rn but it exists)
+        allmoves = new_recurse_material(board, allmoves, MATERIAL_SCAN_DEPTH)
 
         # check the polygot books for openings
         for i in track(range(len(os.listdir("./polygot_openings/"))), description="Loading polygot_openings..."):
@@ -342,7 +192,7 @@ if __name__ == "__main__":
             print(f"White is up {get_material(board)}")
         else:
             print("Tied in material")
-        print_good(board)
+        print_board(board)
         print(f"Computer move: {move}")
         end = time.time()
         print(f"Computer took: " + str(end - start))
