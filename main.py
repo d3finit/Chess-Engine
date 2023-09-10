0d852b112490737e1f031a47a25e8ae7bb5f508f
def__init__ <liam.outlaw@gmail.com>
2023-09-09 20:56:41 -0700

    starting to add endgame code and removed some useless data

--- main.py
+++ main.py
@@ -4,13 +4,19 @@
 # GNU GPL v3.0
 # ------------------------------------
 
-# import required libs
-import chess, chess.svg, chess.pgn, chess.polyglot, chess.engine
-from Console import *
 import os
+import time
+
+import chess
+import chess.engine
+import chess.pgn
+import chess.polyglot
+import chess.svg
 from rich.progress import track, Progress
 from rich.prompt import Prompt
-import time
+import argparse
+
+from Console import *
 from Utils import sort_tuple, convert_to_int, print_board  # local lib for stuff
 
 
@@ -24,76 +30,75 @@ def get_material(board_to_get):
 
 
 # TODO: add alpha / beta pruning here and optimize it
-def recurse_checkmate(board, movestack, limit):
-    with Progress() as progress:
-        d1 = progress.add_task(f"Mate in X, depth {limit} checking...", total=board.legal_moves.count())
+
+def new_recurse_checkmate(board, movestack, depth):
+    if depth > 0:
         for temp_move in board.legal_moves:
-            if len(movestack) >= 1:
-                pass
+            get_material(board)
+
+            temp_board = chess.Board()
+            temp_board.set_fen(board.fen())
+
+            temp_board.push(temp_move)
+
+            if temp_board.is_checkmate():
+                movestack.append([temp_move, 10000])
 
             else:
-                temp_board = chess.Board(fen=board.fen())
-
-                temp_board.push(temp_move)
-
-                progress.update(d1, advance=1)
-
-                movestack = movestack + recurse_checkmate_2(temp_board, movestack, limit - 1, temp_move, progress)
+                try:
+                    movestack = movestack + new_recurse_checkmate(board, movestack, depth - 1)
+                except RecursionError:
+                    print("RecursionError " + str(depth))
 
     return movestack
 
 
-def recurse_checkmate_2(board, movestack, limit, move, progress):
-    if limit == 0:
-        if board.is_checkmate():
-            movestack.append([move, 10000])
-    else:
-        if limit == 1 or limit == 2:
+def new_recurse_material(board, movestack, depth, pathisgood):
+    if depth > 0:
+        if depth > 2 and pathisgood is not True:
             pass
-        else:
-            d1 = progress.add_task(f"Mate in X, depth {limit} checking...", total=board.legal_moves.count())
-
-        if len(movestack) >= 1:
-            pass
-
-        else:
+        elif depth > 2 and pathisgood is True:
             for temp_move in board.legal_moves:
+                get_material(board)
+
                 temp_board = chess.Board()
                 temp_board.set_fen(board.fen())
 
                 temp_board.push(temp_move)
+                # print(temp_board)
+                # input("")
 
-                if limit == 1 or limit == 2:
-                    pass
+                if get_material(temp_board) > get_material(board):
+                    pathisgood = True
+                    movestack.append([temp_move, 1000])
+
                 else:
-                    progress.update(d1, advance=1)
+                    pathisgood = False
 
-                    recurse_checkmate_2(temp_board, movestack, limit - 1, move, progress)
-    return movestack
-
-
-def new_recurse_material(board, movestack, depth):
-    for temp_move in board.legal_moves:
-        get_material(board)
-
-        temp_board = chess.Board()
-        temp_board.set_fen(board.fen())
-
-        temp_board.push(temp_move)
-
-        if get_material(temp_board) > get_material(board):
-            movestack.push([temp_move, 1000])
-            movestack = movestack + new_recurse_material(board, movestack, depth - 1)
-            print(movestack, depth)
-
-    return movestack
+                x3 = new_recurse_material(board, movestack, depth - 1, pathisgood)
+                if x3 is not None:
+                    movestack = movestack + x3
+    else:
+        return movestack
 
 
 if __name__ == "__main__":
-    # PERMVARS
-    MATE_SCAN_DEPTH = 4
-    MATERIAL_SCAN_DEPTH = 15
-    MINIMAX_DEPTH = 3
+    parser = argparse.ArgumentParser(description="Just an example",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-matesd", "--mate-scan-depth", help="how far should the model go in checkmate checking?")
+    parser.add_argument("-materialsd", "--material-scan-depth", help="how far should the model go in material gain/loss checking?")
+    parser.add_argument("-sf", "--use-stockfish", action="store_true", help="have the engine play stockfish?")
+
+    args = parser.parse_args()
+    config = vars(args)
+    print(config)
+
+    
+    MATE_SCAN_DEPTH = int(config["mate_scan_depth"])
+    MATERIAL_SCAN_DEPTH = int(config["material_scan_depth"])
+    USE_STOCKFISH = bool(config["use_stockfish"])
+    
+    computer_moves = 0
 
     # load stonksfish
     engine = chess.engine.SimpleEngine.popen_uci(r".\stockfish\stockfish-windows-x86-64-avx2.exe")
@@ -112,18 +117,24 @@ if __name__ == "__main__":
         print(f"White is up {get_material(board)}")
     else:
         print("Tied in material")
+
+    print("MATERIAL_SCAN_DEPTH: " + str(MATERIAL_SCAN_DEPTH))
     print_board(board)
 
     while True:
         # get user move
-        move = Prompt.ask("Enter your move (UCI format, enter to have Stockfish16 play.)")
-        # move = ""
+        if not USE_STOCKFISH:
+            player_move = Prompt.ask("Enter your move (UCI format, enter to have Stockfish16 play.)")
+        elif USE_STOCKFISH:
+            player_move = ""
+            
         try:
-            while chess.Move.from_uci(move) not in board.legal_moves:
-                move = Prompt.ask("Enter your move (UCI)")
-            first_game.add_variation(chess.Move.from_uci(move))
-            board.push(chess.Move.from_uci(move))
-        except:
+            while chess.Move.from_uci(player_move) not in board.legal_moves:
+                player_move = Prompt.ask("Enter your move (UCI)")
+            first_game.add_variation(chess.Move.from_uci(player_move))
+            board.push(chess.Move.from_uci(player_move))
+        except Exception as e:
+            # print(e)
             result = engine.play(board, chess.engine.Limit(time=0.1))
             board.push(result.move)
 
@@ -134,6 +145,9 @@ if __name__ == "__main__":
             print(f"White is up {get_material(board)}")
         else:
             print("Tied in material")
+
+        print("MATERIAL_SCAN_DEPTH: " + str(MATERIAL_SCAN_DEPTH))
+        # print("Move: " + str(computer_moves))
         print_board(board)
 
         start = time.time()
@@ -142,14 +156,15 @@ if __name__ == "__main__":
         allmoves = []
         blocked_moves = []
 
-        for i in range(1, MATE_SCAN_DEPTH + 1):
-            if len(allmoves) >= 1:
-                pass
-            else:
-                allmoves = allmoves + recurse_checkmate(board, allmoves, i)
+        x1 = new_recurse_checkmate(board, allmoves, MATE_SCAN_DEPTH)
+        if x1 is not None:
+            allmoves = allmoves + x1
+        else:
+            pass
 
-        # better material recurse with ALPHA BETA PRUNING (its trash rn but it exists)
-        allmoves = new_recurse_material(board, allmoves, MATERIAL_SCAN_DEPTH)
+        x2 = new_recurse_material(board, allmoves, MATERIAL_SCAN_DEPTH, True)
+        if x2 is not None:
+            allmoves = allmoves + x2
 
         # check the polygot books for openings
         for i in track(range(len(os.listdir("./polygot_openings/"))), description="Loading polygot_openings..."):
@@ -159,9 +174,17 @@ if __name__ == "__main__":
 
         reccomended_moves = sort_tuple(allmoves)
         reccomended_moves.reverse()
-
-        first_game.add_variation(chess.Move.from_uci(str(reccomended_moves[0][0])))
-        board.push(chess.Move.from_uci(str(reccomended_moves[0][0])))
+        try:
+            first_game.add_variation(chess.Move.from_uci(str(reccomended_moves[0][0])))
+            board.push(chess.Move.from_uci(str(reccomended_moves[0][0])))
+        except IndexError:
+            # ran out of moves
+            print("Ran out of moves")
+            result = engine.analyse(board, chess.engine.Limit(time=1))
+            print(result["score"].black())
+            # x.append(i)
+            # y.append(result["score"].black())
+            # input("i")
         clear()
         print(board)
         # print(f"I reccomend the move: {reccomended_moves[0][0]} with a confidence of {reccomended_moves[0][1]}")
@@ -193,13 +216,34 @@ if __name__ == "__main__":
             print(f"White is up {get_material(board)}")
         else:
             print("Tied in material")
+        print("MATERIAL_SCAN_DEPTH: " + str(MATERIAL_SCAN_DEPTH))
         print_board(board)
+        try:
+            move = str(reccomended_moves[0][0])
+        except IndexError:
+            move = "NONE"
         print(f"Computer move: {move}")
         end = time.time()
-        print(f"Computer took: " + str(end - start))
+        computer_moves += 1
+        print(f"Computer took: " + str(end - start) + " Move: " + str(computer_moves))
 
         if board.outcome() is not None:
             print(board.outcome().termination)
             print("gameover")
             print(first_game)
+            raise KeyboardInterrupt
         # print(f"I reccomend the move: {reccomended_moves[0][0]} with a confidence of {reccomended_moves[0][1]}")
+
+"""# plotting the points
+plt.plot(x, y)
+
+# naming the x axis
+plt.xlabel('Strenght')
+# naming the y axis
+plt.ylabel('stockfish eval (black pov)')
+
+# giving a title to my graph
+plt.title('My first graph!')
+
+# function to show the plot
+plt.show()"""
