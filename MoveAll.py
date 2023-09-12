79514c0af5c5ffb7de741080c521b469bb8a36a8
def__init__ <liam.outlaw@gmail.com>
2023-09-12 13:53:54 -0700

    cleaning files

--- MoveAll.py
+++
@@ -1,7 +0,0 @@
-import os
-from glob import glob
-
-subfolders = [ f.path for f in os.scandir("./test/") if f.is_dir() ]
-
-for folder in subfolders:
-    os.rename(("test/" + folder.split("./test/")[1] + "/" + folder.split("./test/")[1] + ".pgn"), ("full_games/" + folder.split("./test/")[1] + ".pgn"))
